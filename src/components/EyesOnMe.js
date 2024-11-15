1bd515ccc51c326764a4fa13192f9aba2f33ba7a
HP <ularezephaniah@gmail.com>
2024-11-15 17:11:51 +0300

    changes

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+export default  function EyesOnMe() {
+  function handleFocus () {
+console.log("Good!");
+  }
+
+  function  handleBlur () {
+  console.log("Hey! Eyes on me!");
+
+  }
+    return (
+    <button onFocus={handleFocus}  onBlur={handleBlur}>Eyes on me</button>
+)}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,22 @@
+
+
+
+
+
+
+
+
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+
+    function handleChange () {
+        console.log("Entering password...");
+    } 
+
     return (
-        <div></div>
+        <input type="password" onChange={handleChange} />
     )
 }
 
